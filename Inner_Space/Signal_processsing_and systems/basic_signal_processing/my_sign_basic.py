cc7e5cbeca714be20bfa7cd4d5cc79df7a8405c1
SouthskyVela <3070276181@qq.com>
2025-02-24 21:51:41 +0800

    改2

--- Inner_Space/Signal_processsing_and systems/basic_signal_processing/my_sign_basic.py
+++ Inner_Space/Signal_processsing_and systems/basic_signal_processing/my_sign_basic.py
@@ -1,3 +1,4 @@
 import wave_box
-squ_n1=wave_box.square_wave(1.0,1.0)
-squ_n1.squ_plt(start_time=-5.0,total_time=10.0,num=100,endpoint=True)
+#squ_n1=wave_box.square_wave(1.0,1.0)
+#squ_n1.squ_plt(start_time=-5.0,total_time=10.0,num=100,endpoint=True)
+
--- Inner_Space/Signal_processsing_and systems/basic_signal_processing/wave_box.py
+++ Inner_Space/Signal_processsing_and systems/basic_signal_processing/wave_box.py
@@ -16,3 +16,18 @@ class square_wave:
         plt.title('square wave')
         plt.grid(True) #添加网格
         plt.show()
+
+class  sawtooth_wave:
+    def __init__(self,frquency,amplitude):
+        self.freq=frquency
+        self.amp=amplitude
+    def squ_plt(self,start_time,total_time,num,endpoint):
+        x_label=np.linspace(start_time,start_time+total_time,num,endpoint)
+        y_label=self.amp * signal.square(2*np.pi*self.freq*x_label)
+        plt.plot(x_label,y_label)
+        plt.xlabel('Time')
+        plt.ylabel("Amplitude")
+        plt.title('sawtooth wave')
+        plt.grid(True) #添加网格
+        plt.show()
+#锯齿波还没改完啊啊啊啊啊
