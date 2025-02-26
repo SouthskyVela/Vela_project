7fde5af0677cbc9b92286ce0e5e400f737a3f54a
SouthskyVela <3070276181@qq.com>
2025-02-26 22:10:15 +0800

    添加了一些被动触发波形参数
    接下来是设计波形叠加！

--- Inner_Space/Signal_processsing_and systems/basic_signal_processing/my_sign_basic.py
+++ Inner_Space/Signal_processsing_and systems/basic_signal_processing/my_sign_basic.py
@@ -1,4 +1,6 @@
 import wave_box
-#squ_n1=wave_box.square_wave(1.0,1.0)
-#squ_n1.squ_plt(start_time=-5.0,total_time=10.0,num=100,endpoint=True)
-
+squ_n1=wave_box.square_wave(1.0,1.0)
+squ_n1.squ_plt(start_time=-5.0,total_time=10.0,num=100,endpoint=True)
+tooth_n1=wave_box.sawtooth_wave(2.0,2.5)
+tooth_n1.toooth_plt(start_time=-5.0,total_time=10.0,num=50,endpoint=True)
+#记得把相位设置加上！！
--- Inner_Space/Signal_processsing_and systems/basic_signal_processing/wave_box.py
+++ Inner_Space/Signal_processsing_and systems/basic_signal_processing/wave_box.py
@@ -7,6 +7,9 @@ class square_wave:
     def __init__(self,frquency,amplitude):
         self.freq=frquency
         self.amp=amplitude
+
+        self.wave_shape_num=None
+        self.wave_time=None
     def squ_plt(self,start_time,total_time,num,endpoint):
         x_label=np.linspace(start_time,start_time+total_time,num,endpoint)
         y_label=self.amp * signal.square(2*np.pi*self.freq*x_label)
@@ -17,17 +20,27 @@ class square_wave:
         plt.grid(True) #添加网格
         plt.show()
 
+        self.wave_shape_num=y_label
+        self.wave_time=x_label
+
 class  sawtooth_wave:
     def __init__(self,frquency,amplitude):
         self.freq=frquency
         self.amp=amplitude
-    def squ_plt(self,start_time,total_time,num,endpoint):
+        self.wave_shape_num=None
+        self.wave_time=None
+    def toooth_plt(self,start_time,total_time,num,endpoint):
         x_label=np.linspace(start_time,start_time+total_time,num,endpoint)
-        y_label=self.amp * signal.square(2*np.pi*self.freq*x_label)
+        y_label=self.amp * signal.sawtooth(2*np.pi*self.freq*x_label,width=0.25)
         plt.plot(x_label,y_label)
         plt.xlabel('Time')
         plt.ylabel("Amplitude")
         plt.title('sawtooth wave')
         plt.grid(True) #添加网格
         plt.show()
-#锯齿波还没改完啊啊啊啊啊
+
+        self.wave_shape_num=y_label
+        self.wave_time=x_label
+
+#def wave_add(time,*all_wave):
+    
